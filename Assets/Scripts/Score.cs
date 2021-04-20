42a419eb67a78cc5e7afa02a414332f71acef649
GJones8787 <71368383+GJones8787@users.noreply.github.com>
2021-04-20 16:21:05 -0500

    Updated folders for project
    Added 4 required folders to project

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;//string for score
+  
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");//float to string for score
+    }
+}
